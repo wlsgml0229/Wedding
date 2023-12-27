ea3642ac89934e005c8e4b5f6469a02cf232b3d5
wlsgml0229 <wlsgml0229@naver.com>
2023-12-28 01:11:00 +0900

    video 컴포넌트 구현

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -1,12 +1,23 @@
 import classNames from "classnames/bind"
-import styles from "./Heading.module.scss"
+import styles from "./Video.module.scss"
 
 import Section from "@shared/Section"
 
 const cx = classNames.bind(styles)
 
 function Video() {
-  return <Section>Video</Section>
+  return (
+    <Section className={cx("container")}>
+      <video
+        autoPlay={true}
+        muted={true}
+        loop={true}
+        poster="/assets/poster.jpg"
+      >
+        <source src="/assets/main.mp4" type="video/mp4"></source>
+      </video>
+    </Section>
+  )
 }
 
 export default Video
